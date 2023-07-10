b3958a1a14f1c99b355c020c98cfbc6cfbb83a0b
Rozovod <rozovod.store@mail.ru>
2023-07-10 14:25:03 +0300

    3. Авторизация JWT [#9146]

--- src/main/java/ru/job4j/auth/controller/PersonController.java
+++ src/main/java/ru/job4j/auth/controller/PersonController.java
@@ -3,6 +3,7 @@ package ru.job4j.auth.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.auth.domain.Person;
 import ru.job4j.auth.service.PersonService;
@@ -12,11 +13,12 @@ import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/person")
+@RequestMapping("/persons")
 public class PersonController {
     private final PersonService persons;
+    private BCryptPasswordEncoder passwordEncoder;
 
-    @GetMapping("/")
+    @GetMapping("/all")
     public List<Person> findAll() {
         return persons.findAll();
     }
@@ -56,5 +58,11 @@ public class PersonController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
+        persons.create(person);
     }
 }
--- src/main/java/ru/job4j/auth/service/PersonService.java
+++ src/main/java/ru/job4j/auth/service/PersonService.java
@@ -3,6 +3,10 @@ package ru.job4j.auth.service;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.job4j.auth.domain.Person;
 import ru.job4j.auth.repository.PersonRepository;
@@ -10,9 +14,11 @@ import ru.job4j.auth.repository.PersonRepository;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Collections.emptyList;
+
 @Service
 @AllArgsConstructor
-public class PersonService {
+public class PersonService implements UserDetailsService {
     private final PersonRepository personRepository;
     private static final Logger LOG = LoggerFactory.getLogger(PersonRepository.class);
 
@@ -51,5 +57,13 @@ public class PersonService {
 
     public List<Person> findAll() {
         return personRepository.findAll();
+    }
+
+    public UserDetails loadUserByUsername(String login) {
+        Optional<Person> userOptional = personRepository.findByLogin(login);
+        if (userOptional.isEmpty()) {
+            throw new UsernameNotFoundException(login);
+        }
+        return new User(userOptional.get().getLogin(), userOptional.get().getPassword(), emptyList());
     }
 }
